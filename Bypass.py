f049244ceed2662cc96529ba9d7cc8332715b73a
MR DARK <105338726+JISAN-404@users.noreply.github.com>
2022-10-19 13:27:19 +0600

    Create Bypass.py

---
+++ Bypass.py
@@ -0,0 +1,30 @@
+import os, platform,time
+print("\t NOTE : 1ST COPY KEY THEN SENT ME KEY")
+os.system('xdg-open https://www.facebook.com/profile.php?id=100076321219909');time.sleep(1.2)
+try:
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+#####        os.system('xdg-open https://www.facebook.com/FB.KING.MAHIN.NAME.TOH.SONSO')
+
+
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from max import check_my_key
+
+        check_my_key()
+
+
+
+elif bit == "32bit":
+
+        print("\t [x] SRY BRO BYPASS NOY SUPPORT")
